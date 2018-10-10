dd57feefdd3c4b7db4b9f538481677605011ddc7
xiaoqi <962915174@qq.com>
2018-10-10 18:48:45 +0800

    大麦第一次提交

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const server = require('gulp-webserver');
 const sass = require('gulp-sass');
 const webpack = require('webpack-stream');
+const proxy = require('http-proxy-middleware');
 
 gulp.task('packjs', () => {
   return gulp.src('./src/scripts/**/*.js')
@@ -18,7 +19,18 @@ gulp.task('packjs', () => {
           {
             test: /\.html$/,
             use: [ 'string-loader' ]
-          }
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime']
+              }
+            }
+          }     
         ]
       }
     }))
@@ -33,19 +45,19 @@ gulp.task('packscss', () => {
     .pipe(gulp.dest('./dev/styles'))
 })
 
-// copy iconfonts
-gulp.task('copyicons', () => {
-  return gulp.src('./src/iconfonts/**/*')
-    .pipe(gulp.dest('./dev/iconfonts'))
-})
-
 // 启动一个web-server
 gulp.task('server', () => {
   return gulp.src('./dev')
     .pipe(server({
       host: 'localhost',
-      port: 9000,
+      port: 7000,
       livereture:true,
+      middleware:[
+        proxy("/api",{
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        })
+      ]
     }))
 })
 
@@ -55,15 +67,30 @@ gulp.task('copyhtml', () => {
     .pipe(gulp.dest('./dev/'))
 })
 
+// copy iconfonts
+gulp.task('copyicons', () => {
+  return gulp.src('./src/iconfonts/**/*')
+    .pipe(gulp.dest('./dev/iconfonts'))
+})
+
+// copy libs
+gulp.task('copylibs', () => {
+  return gulp.src('./src/libs/**/*')
+    .pipe(gulp.dest('./dev/libs'))
+})
+
+
 
 // 文件修改 watch
 gulp.task('watch', () => {
   gulp.watch('./src/*.html', ['copyhtml'])
   gulp.watch('./src/styles/**/*', ['packscss'])
   gulp.watch('./src/scripts/**/*', ['packjs'])
+  gulp.watch('./src/libs/**/*', ['copylibs'])
+  gulp.watch('./src/mock/**/*', ['copymock'])
 })
 
 // default task
-gulp.task('default', ['packscss', 'packjs','copyicons', 'copyhtml', 'server', 'watch'], () => {
+gulp.task('default', ['packscss', 'packjs', 'copyhtml','copylibs', 'copyicons', 'server', 'watch'], () => {
   console.log('all works!')
 })
