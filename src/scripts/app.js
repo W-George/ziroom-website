f579f89212aa055252385c1fd7ea1fbc25891d0d
xiaoqi <962915174@qq.com>
2018-10-15 08:14:09 +0800

    第三次提交

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,12 @@
 import homeController from"./controllers/home";
-
+import Router from './utils/router';
 import ziruServer from './controllers/ziru-server';
 
-ziruServer.render();
+// 实例化路由
+let router = new Router();
+router.init();
+// 注册路由
+/* 
+    首页路由：#home,homeController.render
+*/
+router.route('#home',homeController.render);
