05e3b9fb2689b0835ea1dd21c53ab98ec2891408
xiaoqi <962915174@qq.com>
2018-10-15 21:35:11 +0800

    自如维修

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-﻿# ziroom-websize
-合作项目
-=======
 # ziroom-website
 ### 项目分工
 OTzhSun：首页
->>>>>>> master
---
+++ src/scripts/controllers/repair.js
@@ -0,0 +1,11 @@
+import repairTpl from '../views/repair.html';
+
+const render = () => {
+    $("#root").html(repairTpl);
+}
+
+
+
+export default {
+    render
+}
