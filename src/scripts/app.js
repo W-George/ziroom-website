9ca478bb9e355b070c0502e6b6d7a22effe78e10
Fairper <1282541898@qq.com>
2018-10-15 20:50:19 +0800

    fail

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,32 +3,15 @@ import Router from './utils/router';
 import ziruServer from './controllers/ziru-server';
 import detailController from "./controllers/detail";
 
-<<<<<<< HEAD
-homeController.render();
-// detailController.render();
-import Router from './utils/router';
-import ziruServer from './controllers/ziru-server';
-import detail from './controllers/detail'
-
-=======
->>>>>>> master
 // 实例化路由
 let router = new Router();
 router.init();
 // 注册路由
 /* 
     首页路由：#home,homeController.render
-<<<<<<< HEAD
-    详情页路由：#detail',detailController.render
-*/
-router.route('#home',homeController.render);
-router.route('#ziruServer',ziruServer.render);
-router.route('#detail',detailController.render);
-=======
     自如服务：#ziruServer,ziruServer.render
     详情页路由：#detail,detailController.render
 */
 router.route('#home',homeController.render);
 router.route('#ziruServer',ziruServer.render);
 router.route('#detail',detailController.render);
->>>>>>> master
