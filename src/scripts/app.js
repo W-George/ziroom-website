b56f517c3a51b6152ec1c23b45c24ec9d68eb773
xiaoqi <962915174@qq.com>
2018-10-16 21:40:09 +0800

    10.16bug修改

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,11 +2,8 @@ import homeController from"./controllers/home";
 import Router from './utils/router';
 import ziruServer from './controllers/ziru-server';
 import detailController from "./controllers/detail";
-<<<<<<< HEAD
 import repairController from "./controllers/repair";
-=======
 import moreController from "./controllers/more";
->>>>>>> 968f5d1bfd0b3cafdcda7f03fbe3a3297304d896
 
 // 实例化路由
 let router = new Router();
@@ -17,12 +14,10 @@ router.init();
     自如服务路由：#ziruServer,ziruServer.render
     详情页路由：#detail,detailController.render
     自如维修路由：#repair,repairController.render
+    列表页：#more,moreController.render
 */
 router.route('#home',homeController.render);
 router.route('#ziruServer',ziruServer.render);
 router.route('#detail',detailController.render);
-<<<<<<< HEAD
 router.route('#repair',repairController.render);
-=======
 router.route('#more',moreController.render);
->>>>>>> 968f5d1bfd0b3cafdcda7f03fbe3a3297304d896
--- src/scripts/models/detail.js
+++ src/scripts/models/detail.js
@@ -1,18 +1,11 @@
 const list = () => {
     return $.ajax({
-<<<<<<< HEAD
       url: '/api/v7/room/detail.json?city_code=110000&id=60793779',
       success: (result) => {
         return result
-=======
-      url: '/v7/room/detail.json?city_code=110000&id=60793779',
-      success: (result) => {  
-        return result;
->>>>>>> 968f5d1bfd0b3cafdcda7f03fbe3a3297304d896
       }
     })
-  }
-   
+  } 
 export default {
 list
 }
