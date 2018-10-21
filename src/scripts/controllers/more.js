84246d7cb35bde63872391254cae94f9c8c0132e
TIMEZR <2545495010@qq.com>
2018-10-21 13:08:59 +0800

    111

--- src/scripts/controllers/more.js
+++ src/scripts/controllers/more.js
@@ -8,17 +8,24 @@ var pageNo = 1;
 const render = async () => {
     // 渲染页面
     $("#root").html(moreTpl);
+    let list = (await moreModel.data()).data.rooms;
+    console.log(list);
     // 动态渲染数据
-    renderData();
+    await renderData(list);
     // 滚动
     scroll();
+    // 鼠标滑过
+    move()
+    // 滚动附近
+    scrollFj()
+    
+    mousenter()
 
 }
 
 // 动态渲染数据
-const renderData = async () => {
-    let result = await moreModel.data();
-    let list=datasource = result.data.rooms;
+const renderData = async (list) => {
+
     let template = Handlebars.compile(morelistTpl)
     let html = template({
         list
@@ -26,13 +33,13 @@ const renderData = async () => {
     $('.main').html(html)
 }
 
-// 滚动
+// 滚动list
 const scroll = async () => {
     let posScroll = new BScroll(".scroll", {
         probeType: 2
     })
 
-    // 最大滚动
+
     // 监听滚动条滚动的实时位置
     posScroll.on('scrollEnd', async function () {
         let y = this.y,
@@ -44,8 +51,9 @@ const scroll = async () => {
             let result = await moreModel.load(++pageNo);
             let list = datasource = [
                 ...datasource,
-                ...result.data.rooms,
-              ]
+                ...result.data.rooms
+              ];
+            renderData(list);
               console.log(list)
             this.refresh()
             this.scrollTo(0, this.maxScrollY + 40)
@@ -55,7 +63,62 @@ const scroll = async () => {
 
 }
 
+// 鼠标滑过ul
+const move = function(){
+    let flag = true;
 
+    $('.nav li').eq(0).addClass('act')
+
+    let indexA = 0;
+
+    $('.nav li').on('tap',function(){
+        // alert()
+        var index=$(this).index()
+        // console.log(index)
+        if(indexA == index){
+
+            if(flag){
+                
+                $(this).addClass('act').siblings().removeClass('act')
+                // 点击上面的切换下面的
+                $('.filter-hov').eq(index).css('display','block').siblings().css('display','none');
+        
+                flag = false;
+        
+                }else{
+        
+                    flag = true;
+        
+                    $('.filter-hov').eq(index).css('display','none')
+                }
+        }else{
+            $(this).addClass('act').siblings().removeClass('act')
+            // 点击上面的切换下面的
+            $('.filter-hov').eq(index).css('display','block').siblings().css('display','none');
+        }
+       
+
+       indexA = index;
+    })
+}
+
+// 滚动附近
+const scrollFj = async () => {
+    let posScroll = new BScroll(".weiz-right", {
+        probeType: 2
+    })
+}
+
+const mousenter = function(){
+    $('.weiz_right_p p').on('tap',function(){
+        // console.log($(this).index())
+        $(this).css("color","#ffa000").siblings().css("color","")
+    })
+
+    $('.weiz-left-div').on("tap",function(){
+        $(this).css("color","#ffa000").siblings().css("color","")
+    })
+}
 export default {
     render
 }
